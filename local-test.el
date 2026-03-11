5b2fbca60c466e9a0788e07ff6342bcf6815a758
heartnheart <xlzhang921@gmail.com>
2026-03-11 16:32:16 +0800

    add more test statement

--- local-test.el
+++ local-test.el
@@ -2,12 +2,13 @@
 
 (setq debug-on-error t)
 (add-to-list 'load-path (file-name-directory (or load-file-name (buffer-file-name))))
+(setq ewv-browser-user-data-folder (expand-file-name "myuserdir" invocation-directory))
+
 (require 'ewv)
 ;; https://github.com/prem-k-r/MaterialYouNewTab
 (setq ewv-browser-default-url "extension://jjpokbgpiljgndebfoljdeihhkpcpfgl/index.html")
 
 (setq ewv-browser-default-url "https://www.baidu.com")
 
-; (setq ewv-browser-user-data-folder (expand-file-name "myuserdir" invocation-directory))
 
 (add-hook 'emacs-startup-hook #'ewv-browser-reopen-all-pinned-tabs)
